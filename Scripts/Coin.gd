a7eefe2f0792253d048358a51a680ccce48fc4c9
LvanArkel <lvanarkel@hotmail.nl>
2022-06-04 13:30:40 +0200

    Refactored test sprites and added weapon switching/store

---
+++ Scripts/Coin.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+export (int) var value = 10
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Coin_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.is_in_group("Player"):
+		print("Picking up coin")
+		body.pickup_coin(value)
+		queue_free()
